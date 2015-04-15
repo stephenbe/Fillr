ae8c97640df483a4f02f9f2a86b4de2d6a544687
Stephen Bégay <stephenbe292+github@gmail.com>
2015-04-14 21:33:54 -0400

    when page load redefine current slide
    I crated a new function :
    defineCurrentSlide : it redefines the current slide with the closest
    slide from the top of window

--- fillr.js
+++ fillr.js
@@ -1,154 +1,167 @@
 ;(function ( $, window, document, undefined ) {
 
-    "use strict";
+	"use strict";
 
-    var fillr = "fillr";
+	var fillr = "fillr";
 
-    var defaults = {
-        dots: true,
-        slide: '.slide',
-        previous_btn : '.js-fillr-previous',
-        next_btn : '.js-fillr-next'
-    };
+	var defaults = {
+		dots: true,
+		slide: '.slide',
+		previous_btn : '.js-fillr-previous',
+		next_btn : '.js-fillr-next'
+	};
 
-    var initials = {
-        currentSlide : $(".slide").first(),
-        currentSlideId : $(".slide").first().index(),
-        is_animating : false
-    };
+	var initials = {
+		currentSlide : $(".slide").first(),
+		currentSlideId : $(".slide").first().index(),
+		is_animating : false
+	};
 
-    /**
-    * Plugin Constructor
-    */
-    function Fillr ( element, options ) {
-        self = this;
-        this.element = element;
+	/**
+	* Plugin Constructor
+	*/
+	function Fillr ( element, options ) {
+		self = this;
+		this.element = element;
 
-        this.settings = $.extend( {}, defaults, options, initials );
-        this._defaults = defaults;
-
-
-        this.init();
-    };
+		this.settings = $.extend( {}, defaults, options, initials );
+		this._defaults = defaults;
 
 
-    Fillr.prototype.init = function () {
-        //add listeners
-        $(self.settings.previous_btn).on("click",  self.goToPrevSlide);
-        $(self.settings.next_btn).on("click",   self.goToNextSlide);
-        $(window).on("mousewheel DOMMouseScroll", self.onMousewheel);
-        $(window).scroll(self.onScroll);    
-    };
+		this.init();
+	};
 
-    /**
-    * Checks for children of self
-    *
-    * @return number of slides (int)
-    */
-    Fillr.prototype.numberOfSlides = function() {
-        return $(self.element).children().length;
-    };
 
-    /**
-    * Creates html for dots
-    *
-    * @return the html that makes the nav
-    */
-    Fillr.prototype.buildDots = function() {
-        if (true) {
+	Fillr.prototype.init = function () {
+		//add listeners
+		$(self.settings.previous_btn).on("click",  self.goToPrevSlide);
+		$(self.settings.next_btn).on("click",   self.goToNextSlide);
+		$(window).on("mousewheel DOMMouseScroll", self.onMousewheel);
+		$(window).scroll(self.onScroll);    
 
-        }
-    };
+		if ($(window).scrollTop() > 0) {
+			self.defineCurrentSlide();
+		};
+	};
 
-    /**
-    * Goes to next slide
-    *
-    */
-    Fillr.prototype.goToNextSlide = function() {
-        if(self.settings.currentSlide.next().length){
-            self.goToSlide(self.settings.currentSlide.first().next());
-        }
-    };
+	/**
+	* Checks for children of self
+	*
+	* @return number of slides (int)
+	*/
+	Fillr.prototype.numberOfSlides = function() {
+		return $(self.element).children().length;
+	};
 
-    /**
-    * Goes to prev slide
-    *
-    */
-    Fillr.prototype.goToPrevSlide = function() {
-        if(self.settings.currentSlide.prev().length) {
-            self.goToSlide(self.settings.currentSlide.first().prev());
-        }
-    };
+	/**
+	* Creates html for dots
+	*
+	* @return the html that makes the nav
+	*/
+	Fillr.prototype.buildDots = function() {
+		if (true) {
 
-    /**
-    * Goes to indicated slide
-    *
-    */
-    Fillr.prototype.goToSlide = function($slide) {        
-        if (self.settings.is_animating != true) {
-            self.settings.currentSlide = $slide;
-            self.settings.currentSlideId = self.settings.currentSlide.index();
-            self.settings.is_animating = true;
+		}
+	};
 
-            $("body, html").animate(
-                {	 
-                	scrollTop: $(self.settings.slide).eq(self.settings.currentSlideId).offset().top,
-                },
-                {
-                    duration:1500,
-                    easing: "easeInOutCirc",
-                    complete: function() {
-                        self.settings.is_animating = false; 
-                    }
-                }
-            );
-        }
-    }
+	/**
+	* Goes to next slide
+	*
+	*/
+	Fillr.prototype.goToNextSlide = function() {
+		if(self.settings.currentSlide.next().length){
+			self.goToSlide(self.settings.currentSlide.first().next());
+		}
+	};
+
+	/**
+	* Goes to prev slide
+	*
+	*/
+	Fillr.prototype.goToPrevSlide = function() {
+		if(self.settings.currentSlide.prev().length) {
+			self.goToSlide(self.settings.currentSlide.first().prev());
+		}
+	};
+
+	/**
+	* Goes to indicated slide
+	*
+	*/
+	Fillr.prototype.goToSlide = function($slide) {        
+		if (self.settings.is_animating != true) {
+			self.settings.currentSlide = $slide;
+			self.settings.currentSlideId = self.settings.currentSlide.index();
+			self.settings.is_animating = true;
+
+			$("body, html").animate(
+				{	 
+					scrollTop: $(self.settings.slide).eq(self.settings.currentSlideId).offset().top,
+				},
+				{
+					duration:1500,
+					easing: "easeInOutCirc",
+					complete: function() {
+						self.settings.is_animating = false; 
+					}
+				}
+			);
+		}
+	}
   
-    Fillr.prototype.onMousewheel = function(event) {
+	Fillr.prototype.onMousewheel = function(event) {
 
-        //Normalize event wheel delta
-        var delta = event.originalEvent.wheelDelta / 30 || -event.originalEvent.detail;
+		//Normalize event wheel delta
+		var delta = event.originalEvent.wheelDelta / 30 || -event.originalEvent.detail;
 
-        event.preventDefault();  
+		event.preventDefault();  
 
-        //If the user scrolled up, it goes to previous slide, otherwise - to next slide
-        if(delta < -1) {
-            self.goToNextSlide();
-        }
-        else if(delta > 1) {
-            self.goToPrevSlide();
-        }
-    };
+		//If the user scrolled up, it goes to previous slide, otherwise - to next slide
+		if(delta < -1) {
+			self.goToNextSlide();
+		}
+		else if(delta > 1) {
+			self.goToPrevSlide();
+		}
+	};
 
-    Fillr.prototype.onScroll = function(event) {
-        clearTimeout($.data(this, 'scrollTimer'));
-        $.data(this, 'scrollTimer', setTimeout(function() {
-            var found=null;
-            var found_top=0;
+	Fillr.prototype.defineCurrentSlide = function(event) {
+		var found=null;
+		var found_top=0;
 
-            $(self.settings.slide).each(function(index, el) {
-                var $this = $(this);
-                var offset = Math.abs($this.offset().top - $(window).scrollTop());          	
+		$(self.settings.slide).each(function(index, el) {
+			var $this = $(this);
+			var offset = Math.abs($this.offset().top - $(window).scrollTop());              
 
-                if((found == null ) || ( offset >= 0 ) && ( offset < found_top )){
-                    found = $this;
-                    found_top = offset;
-                }
-            });
-            self.goToSlide(found);
 
-        }, 2000));			
-    }
+			//find the closest slide to the top
+			if((found == null ) || ( offset >= 0 ) && ( offset < found_top )){
+				found = $this;
+				found_top = offset;
+			}
+		});  
 
-    /**
-    * Create the jquery plugin function
-    */
-    $.fn.fillr = function ( options ) {
-        return this.each(function() {
-        if ( !$.data( this, "plugin_" + fillr ) ) {
-            $.data( this, "plugin_" + fillr, new Fillr( this, options ) );
-        }
-        });
-    };
+		self.settings.currentSlide = found;
+	}
+
+	Fillr.prototype.onScroll = function(event) {
+		clearTimeout($.data(this, 'scrollTimer'));
+		$.data(this, 'scrollTimer', setTimeout(function() {
+			self.defineCurrentSlide();
+			
+			self.goToSlide(self.settings.currentSlide);
+
+		}, 2000));			
+	}
+
+	/**
+	* Create the jquery plugin function
+	*/
+	$.fn.fillr = function ( options ) {
+		return this.each(function() {
+		if ( !$.data( this, "plugin_" + fillr ) ) {
+			$.data( this, "plugin_" + fillr, new Fillr( this, options ) );
+		}
+		});
+	};
 })( jQuery, window, document );
